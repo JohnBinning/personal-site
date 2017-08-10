1f89b8bda6803aef2f35ef224c7f42fd61c80e42
John Binning <john.binning17@gmail.com>
2017-08-10 13:12:40 -0600

    Add about

---
+++ src/Components/About/About.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <article className="about">
+      <h3>About Me</h3>
+      <p>
+        I am a something
+      </p>
+    </article>
+  )
+}
+
+export default About;
