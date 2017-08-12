38cdcb8a1039fd6917d120217492e7f75ad82120
John Binning <john.binning17@gmail.com>
2017-08-11 19:02:12 -0600

    add wanderlist and images to projects

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -6,13 +6,15 @@ export const NavBar = () => {
   return (
     <article className='navbar'>
       <div className='inner-nav'>
-        <img className='logo' alt="John's logo" src='images/logo.png'/>
+        {/*<img className='logo' alt="John's logo" src='images/logo.png'/>*/}
         <h1>John Binning</h1>
         <h5>Front-End Developer</h5>
-        <NavLink to='/About' className='aside-nav' activeClassName='selected'>About</NavLink>
-        <NavLink to='/Projects' className='aside-nav' activeClassName='selected'>Projects</NavLink>
-        <NavLink to='/Resume' className='aside-nav' activeClassName='selected'>Resume</NavLink>
-        <NavLink to='/Contact' className='aside-nav' activeClassName='selected'>Contact</NavLink>
+        <div className='nav-link-wrapper'>
+          <NavLink to='/About' className='aside-nav' activeClassName='selected'>About</NavLink>
+          <NavLink to='/Projects' className='aside-nav' activeClassName='selected'>Projects</NavLink>
+          <NavLink to='/Resume' className='aside-nav' activeClassName='selected'>Resume</NavLink>
+          <NavLink to='/Contact' className='aside-nav' activeClassName='selected'>Contact</NavLink>
+        </div>
       </div>
     </article>
   )
