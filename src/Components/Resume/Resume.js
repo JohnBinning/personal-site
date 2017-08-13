6654fe37e9af27b1bd7336a73921310a023eac39
John Binning <john.binning17@gmail.com>
2017-08-13 15:06:21 -0600

    add experience to resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -6,12 +6,12 @@ const Resume = () => {
     <article className="resume">
       <h2>Resume</h2>
       <figure>
-        <h3>Projects</h3>
+        <h3>PROJECTS</h3>
         <Link to='/projects' ><div>See Projects</div></Link>
       </figure>
 
       <figure>
-        <h3>Skills</h3>
+        <h3>SKILLS</h3>
         <div className='skills-container'>
           <p className='resume-skill'>Javascript</p>
           <p className='resume-skill'>HTML5 & CSS3</p>
@@ -25,6 +25,43 @@ const Resume = () => {
           <p className='resume-skill'>WAI-ARIA</p>
           <p className='resume-skill'>Responsive UX/UI</p>
         </div>
+      </figure>
+      <figure>
+
+        <h3>EXPERIENCE</h3>
+
+        <div className='experience-container'>
+          <div className='job-container'>
+            <h4 className='job-title'>Human Resources Intake Coordinator</h4>
+            <div className='employer'>PASCO Home Health / Lakewood, CO</div>
+            <p className='job-duties'>I organized collaboration between various departments to streamline our intake process. I also trained parents of children with disabilities and helped them coordinate with the government to receive services.</p>
+          </div>
+        </div>
+
+        <div className='experience-container'>
+          <div className='job-container'>
+            <h4 className='job-title'>Operations/Claim Finance</h4>
+            <div className='employer'>Travelers Insurance / Greenwood Village, CO</div>
+            <p className='job-duties'>I audited payments made to and from the Travelers Claims department, ensuring that they met government imposed financial guidelines.</p>
+          </div>
+        </div>
+
+        <div className='experience-container'>
+          <div className='job-container'>
+            <h4 className='job-title'>Claims Technician</h4>
+            <div className='employer'>The Hartford / Hartford, CT</div>
+            <p className='job-duties'>I managed claim information distribution for the Northeast and Southeast US. I also researched and developed efficient cient claim handling on the Process and Procedures Council.</p>
+          </div>
+        </div>
+
+        <div className='experience-container'>
+          <div className='job-container'>
+            <h4 className='job-title'>History and Writing Instructor</h4>
+            <div className='employer'>Grace Academy / New Haven, CT</div>
+            <p className='job-duties'>I developed and taught lesson plans in history, writing, and grammar. I tutored children in both an individual and a classroom setting.</p>
+          </div>
+        </div>
+
       </figure>
 
     </article>
