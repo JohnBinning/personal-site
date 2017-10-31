87f3e24cfbcd7378c3ee7ca95c4a9cb3cc049836
John Binning <john.binning17@gmail.com>
2017-10-31 14:38:23 -0600

    add mentr to projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -37,6 +37,35 @@ const Projects = () => {
 
         <figure className='project-wrapper'>
           <figcaption>
+            <h3 className='project-title'>Mentr </h3>
+            <img className='project-image' src='images/mentr.gif' alt='mentr preview'/>
+            <div className='project-links'>
+              <a href='https://github.com/JohnBinning/mentr' className='project-github project-links'>
+                <img src='images/github.png' className='project-link-social' alt='github logo link' />
+                {'GitHub'}
+              </a>
+              <a href='https://turing-mentr.herokuapp.com/' className='project-hosted project-links'>
+                <img src='images/internet.png' className='project-link-social' alt='live site link' />
+                {'Live Site'}
+              </a>
+            </div>
+            <p className='project-description'>
+              Mentr was a group project created in one week during my final module as a student at the Turing School of Software and Design.
+              It is an app that allows Turing students and volunteer mentors to connect.  The intent was to create a user friendly interface 
+              on what was previously an intimidating Google Docs spreadsheet.  We authenticated users through GitHub Oauth, and used a PostgreSQL DB. 
+              I worked with Leta Keane and Spencer Hilvitz.
+            </p>
+            <div className='project-stack'>
+              <h4 className='the-stack'>The Stack:</h4>
+              <p className='front-end'><strong>Front End: </strong><span>React</span></p>
+              <p><strong>Back End: </strong><span>NodeJS, Express</span></p>
+              <p><strong>Testing: </strong><span>Mocha, Chai</span></p>
+            </div>
+          </figcaption>
+        </figure>
+
+        <figure className='project-wrapper'>
+          <figcaption>
             <h3 className='project-title'>Movie Tracker </h3>
             <img className='project-image' src='images/movieTracker.gif' alt='movieTracker preview'/>
             <div className='project-links'>
