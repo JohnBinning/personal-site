f950ebe1baea466a7a60759f389bd4efe89d89e2
John Binning <john.binning17@gmail.com>
2017-11-14 15:54:43 -0700

    remove unused service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory'
 import App from './Components/App/App.js';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 const history = createHistory();
