e6ae0d155f49113973aa54cd55d6eb8f519df758
John Binning <john.binning17@gmail.com>
2017-12-21 20:02:57 -0700

    update social links to remove extra margin

--- src/Components/Social/Social.js
+++ src/Components/Social/Social.js
@@ -6,7 +6,7 @@ export const Social = () => {
   return (
     <article className='social-links'>
       <a href='https://github.com/johnbinning' className='social-links' target='_blank' role='link'>
-        <img src='images/github.png' className='github social-img left-soc' alt='github logo link' />
+        <img src='images/github.png' className='github social-img' alt='github logo link' />
       </a>
       <a href='https://www.linkedin.com/in/john-binning/' target='_blank' className='social-links' role='link'> 
         <img src='images/linkedin.png' className='linkedin social-img' alt='linkedin logo link' />
