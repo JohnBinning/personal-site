afc99d4b1266b73be9038053ab33a44c6ee2b103
John Binning <john.binning17@gmail.com>
2017-12-21 20:00:05 -0700

    change to in link to href in a for social github

--- src/Components/Social/Social.js
+++ src/Components/Social/Social.js
@@ -5,7 +5,7 @@ export const Social = () => {
 
   return (
     <article className='social-links'>
-      <a to='https://github.com/johnbinning' className='social-links' target='_blank' role='link'>
+      <a href='https://github.com/johnbinning' className='social-links' target='_blank' role='link'>
         <img src='images/github.png' className='github social-img left-soc' alt='github logo link' />
       </a>
       <a href='https://www.linkedin.com/in/john-binning/' target='_blank' className='social-links' role='link'> 
