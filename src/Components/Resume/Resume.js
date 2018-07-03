ce354c622fb3ef246eb80620c574e2a97f3b42b5
John Binning <john.binning17@gmail.com>
2018-07-03 14:28:03 -0600

    add to skills

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -5,7 +5,6 @@ import '../../stylesheets/Resume.css';
 const Resume = () => {
   return (
     <article className="resume">
-      {/*<h2 className='location-title'>Resume</h2>*/}
       <figure>
         <h3 className='title'>PROJECTS</h3>
         <Link to='/projects' className='p-link' ><div className='grey-block projects-link resume-blocks'>Click to See Projects</div></Link>
@@ -16,11 +15,12 @@ const Resume = () => {
           <p className='resume-skill'>Javascript</p>
           <p className='resume-skill'>HTML5 & CSS3</p>
           <p className='resume-skill'>React/Redux</p>
+          <p className='resume-skill'>Knockout JS</p>
           <p className='resume-skill'>jQuery</p>
-          <p className='resume-skill'>NodeJS</p>
+          <p className='resume-skill'>Node JS</p>
           <p className='resume-skill'>Express</p>
           <p className='resume-skill'>PostgreSQL</p>
-          <p className='resume-skill'>Testing: Mocha, Chai, Enzyme, Jest</p>
+          <p className='resume-skill'>Testing: Mocha, Chai, QUnit, Enzyme, Jest</p>
           <p className='resume-skill'>Git and Agile Workflow</p>
           <p className='resume-skill'>WAI-ARIA</p>
           <p className='resume-skill'>Responsive UX/UI</p>
